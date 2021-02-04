fff3db4c0e63c56a1182dcecf343ec3a4ad93809
Corama Yunus <coramayunus@gmail.com>
2021-02-04 14:26:39 +0300

    Restructuring to use socket.io

--- await.mjs
+++ await.mjs
@@ -9,3 +9,25 @@ async function printID() {
   console.log(ids);
 }
 printID();
+
+const bigData = {
+  name: "Big data",
+  whatItDoes: () => {
+    console.log("gathers the data information");
+  },
+  subBranch: {
+    branch1: {
+      location: "Kigoma",
+      employees: 3,
+    },
+    branch1: {
+      location: "Arusha",
+      employees: 6,
+    },
+  },
+  bigDataInfo: () => {
+    bigData.whatItDoes();
+    // bigData.bigDataInfo();
+  },
+};
+bigData.bigDataInfo();
---
+++ js/value.js
@@ -0,0 +1,5 @@
+const num = [ 3, 6, 9 ];
+
+num.push(num.push(num.pop( )));
+
+console.log(num);
